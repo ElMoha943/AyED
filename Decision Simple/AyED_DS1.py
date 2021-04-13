f934c7312fa7c638b0cc452b18a436dd2a0ed6f1
Valen <valentinodidio943@gmail.com>
2021-04-13 17:35:13 -0300

    Delete Decision Simple directory

--- Decision Simple/AyED_DS1.py
+++
@@ -1,6 +0,0 @@
-n1 = input("Ingresa el primer numero \n" )  
-n2 = input("Ingresa el segundo numero \n" )  
-if n1 > n2:
-    print(n2,n1)
-else:
-    print(n1,n2)
--- Decision Simple/AyED_DS5.py
+++
@@ -1,7 +0,0 @@
-n1 = input("Ingresa el primer numero \n" )  
-n2 = input("Ingresa el segundo numero \n" )  
-n3 = input("Ingresa el tercer numero \n" ) 
-if n1<n2 and n1<n3:
-    print('Es menor')
-else:
-    print('No es menor')
