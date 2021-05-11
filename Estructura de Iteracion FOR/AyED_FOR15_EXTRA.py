308d201404f4df56c81d5799e5ac697505d8c8dd
Valen <valentinodidio943@gmail.com>
2021-05-11 19:23:35 -0300

    Delete AyED_FOR15_EXTRA.py

--- Estructura de Iteracion FOR/AyED_FOR15_EXTRA.py
+++
@@ -1,8 +0,0 @@
-#Nota: este ejercicio esta resuelto utilizando listas (arrays), cosa que no dimos en clase, pero lo dejo para el que pueda interezarle.
-Fibonacci = []
-for i in range(23):
-    if i >= 2:
-        Fibonacci.append(Fibonacci[i-1]+Fibonacci[i-2])
-    else:
-        Fibonacci.append(1)
-    print(Fibonacci[i])
