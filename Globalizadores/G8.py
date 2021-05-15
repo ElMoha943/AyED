2c4480616cf160c6505a8180c0b5ec88a462a994
Valen <valentinodidio943@gmail.com>
2021-05-15 18:51:06 -0300

    Update G8.py
    Fixed

--- Globalizadores/G8.py
+++ Globalizadores/G8.py
@@ -1,4 +1,7 @@
-total = 1
-for i in range (1,65):
-    if i!=1: total+=total*2
+total = 0
+anterior = 1
+for i in range (2,65):
+    actual=anterior*2
+    total +=actual
+    anterior=actual
 print(f"Dberia cobrar {total+1} granos")
